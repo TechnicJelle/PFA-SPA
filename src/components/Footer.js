82d6608a42758a9837323baa469a33fbe3eab517
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-03-14 21:01:56 +0100

    Link to the website source code repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,9 @@ export default class Footer extends React.Component {
             <footer className="footer">
                 <div className="content has-text-centered">
                     <p>
-                        <strong><a href="https://github.com/TechnicJelle/Project-Final-Approach">Catoms</a></strong> by <strong>The Atoms Family</strong>. The source code is licensed
-                        <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                        <strong><a href="https://github.com/TechnicJelle/Project-Final-Approach">Catoms</a></strong> by <strong>The Atoms Family</strong>.
+                        The source code is licensed <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
+                        The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a> and available <a href="https://github.com/TechnicJelle/PFA-SPA">here</a>.
                     </p>
                 </div>
             </footer>
